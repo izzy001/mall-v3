09fb535d899a9e169db75d094f3bfdfdea006d00
Izzy <israelemoi@gmail.com>
2022-01-14 11:28:32 +0100

    fixed reset password bug

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -9,12 +9,13 @@ import { Otp } from '../models/otp.model';
 import { TwoFA } from '../models/2FA.model';
 
 export const sendOtp = async (req: any, res: any) => {
-  const otpEmailExists =  await Otp.findOne({email: req.body.email});
-   if(otpEmailExists?.verified === true) return res.status(403).send({
-       message: "This user is verified!...Kindly proceed to register user"});
+    const otpEmailExists = await Otp.findOne({ email: req.body.email });
+    if (otpEmailExists?.verified === true) return res.status(403).send({
+        message: "This user is verified!...Kindly proceed to register user"
+    });
 
-    if(otpEmailExists?.verified === false) {
-        await Otp.deleteOne({ email: req.body.email});
+    if (otpEmailExists?.verified === false) {
+        await Otp.deleteOne({ email: req.body.email });
     }
 
     //attempting to generate otp
@@ -39,57 +40,57 @@ export const sendOtp = async (req: any, res: any) => {
 //check if 2FA is set by user
 export const check2FAStatus = async (req: any, res: any) => {
 
-    const emailExistsInRecord = await User.findOne({email: req.body.email});
-    if(!emailExistsInRecord) return res.status(400).send('This email does not exist in our records');
+    const emailExistsInRecord = await User.findOne({ email: req.body.email });
+    if (!emailExistsInRecord) return res.status(400).send('This email does not exist in our records');
 
     console.log(`This is user record ${emailExistsInRecord}`)
 
     const status = emailExistsInRecord.two_factor_authentication;
-     return res.send({
-         "message": "2FA status retrieved successfully",
-         "status": status
-     });
+    return res.send({
+        "message": "2FA status retrieved successfully",
+        "status": status
+    });
 };
 
 
 export const send2FACode = async (req: any, res: any) => {
 
-    const twoFAEmailExists =  await TwoFA.findOne({email: req.body.email});
-    
-      if(twoFAEmailExists) {
-          await TwoFA.deleteOne({ email: req.body.email});
-      }
+    const twoFAEmailExists = await TwoFA.findOne({ email: req.body.email });
 
-      const emailExistsInRecord = await User.findOne({email: req.body.email});
-      if(!emailExistsInRecord) return res.status(400).send('This email does not exist in our records');
-  
-      //attempting to generate otp
-      //Generate OTP 
-      const otp = otpGenerator.generate(6, { alphabets: false, upperCase: false, specialChars: false });
-  
-      //create OTP instance in DB
-      const twoFA_instance = await TwoFA.create({
-          email: req.body.email,
-          otp: otp,
-          expiration_time: Date.now() + 300000
-      });
-  
-      const verification_token = twoFA_instance.generate2FAToken();
-      res.send({
-          message: "Otp has been successfully sent to user",
-          details: verification_token,
-          otp: otp
-      });
-  };
+    if (twoFAEmailExists) {
+        await TwoFA.deleteOne({ email: req.body.email });
+    }
 
-  export const verify2FAToken = async (req: any, res: any) => {
+    const emailExistsInRecord = await User.findOne({ email: req.body.email });
+    if (!emailExistsInRecord) return res.status(400).send('This email does not exist in our records');
+
+    //attempting to generate otp
+    //Generate OTP 
+    const otp = otpGenerator.generate(6, { alphabets: false, upperCase: false, specialChars: false });
+
+    //create OTP instance in DB
+    const twoFA_instance = await TwoFA.create({
+        email: req.body.email,
+        otp: otp,
+        expiration_time: Date.now() + 300000
+    });
+
+    const verification_token = twoFA_instance.generate2FAToken();
+    res.send({
+        message: "Otp has been successfully sent to user",
+        details: verification_token,
+        otp: otp
+    });
+};
+
+export const verify2FAToken = async (req: any, res: any) => {
     //check if email is valid
     const isEmailValid = await TwoFA.findOne({ email: req.user.email });
-    if (!isEmailValid) return res.status(400).send({ message: 'This email is not in our records'});
+    if (!isEmailValid) return res.status(400).send({ message: 'This email is not in our records' });
     const isValidOtp = await TwoFA.findOne({ otp: req.body.otp });
-    if (!isValidOtp) return res.status(400).send({ message: 'This token is invalid! Resend OTP'});
+    if (!isValidOtp) return res.status(400).send({ message: 'This token is invalid! Resend OTP' });
     if (new Date() > new Date(req.user.expiration_time)) return res.status(400).send(
-        { message: 'This token is expired! Resend OTP'});
+        { message: 'This token is expired! Resend OTP' });
     if (req.body.otp == req.user.otp) return res.send({ message: 'Otp verified succesfully!' });
 };
 
@@ -178,27 +179,31 @@ export const requestPasswordReset = async (req: any, res: any) => {
         .send('Sorry! We do not have this email in our records. Please try again with email registered on altmall');
 
     //Check for existing Token
-     let token = await Token.findOne({ user_id: user._id });
-     if (token) await token.deleteOne();
+    let token = await Token.findOne({ user_id: user._id });
+    if (token) await token.deleteOne();
 
-    
+
     let resetToken = crypto.randomBytes(10).toString('hex'); //Math.random().toString(36).substr(2, 5); //crypto.randomBytes(32).toString('hex');
     console.log(`This is the reset token generated ${resetToken}`);
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(resetToken, salt);
+    const saltRounds = 10;
+    //const salt = await bcrypt.genSalt(saltRounds);
+    await bcrypt.hash(resetToken, saltRounds, async function (err, hash) {
+        await new Token({
+            user_id: user._id,
+            token: hash,
+            createdAt: Date.now()
+        }).save();
+        console.log(`This is the hashed reset token : ${hash}`);
+        const link = `http://localhost:8000/passwordReset?token=${resetToken}&id=${user._id}`;
+        return res.send(link);
+    });
 
-    console.log(`This is the hashed reset token : ${hash}`);
-    await new Token({
-        user_id: user._id,
-        token: hash,
-        createdAt: Date.now()
-    }).save();
+
+
 
     // const link = `${clientURL}/passwordReset?token=${resetToken}&id=${user._id}`;
-    const link = `http://localhost:8000/passwordReset?token=${hash}&id=${user._id}`;
 
     //TODO : email sending via Template to customers: waiting for templates from Cx
-    res.send(link);
 }
 
 export const resetPassword = async (req: any, res: any) => {
@@ -208,17 +213,14 @@ export const resetPassword = async (req: any, res: any) => {
     });
 
     //Check if token exist
-    const passwordResetToken = await Token.findOne({user_id: req.body.user_id});
-
-    console.log(`This is a password reset token ${passwordResetToken}`)
+    const passwordResetToken = await Token.findOne({ user_id: req.body.user_id });
     if (!passwordResetToken) return res.status(400).send({
         details: "Invalid or expired password reset token"
     });
 
+
     //check if reset token received matches with token saved to user's profile
-    console.log(`this is the token from request ${req.body.token}`);
     const isValid = await bcrypt.compare(req.body.token, passwordResetToken.token);
-    console.log(isValid);
     if (!isValid) return res.status(400).send({
         details: "Invalid or expired password reset token"
     });
@@ -234,7 +236,9 @@ export const resetPassword = async (req: any, res: any) => {
 
     //TODO: email sending via Template to customers: waiting on Cx
 
-    await passwordResetToken.deleteOne();
-    return true;
+    //await passwordResetToken.deleteOne();
+    return res.send({
+        message: 'Password change is successful'
+    });
 
 }
